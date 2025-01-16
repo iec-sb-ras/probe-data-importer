72f208c7aa45a74ba000b63d7162271c4b7cc057
Evgeny Cherkashin <eugeneai@irnok.net>
2025-01-17 01:24:21 +0800

    Added interpretation of sample names.

--- i_pol.py
+++ i_pol.py
@@ -1,24 +1,43 @@
 # import pandas as pd
 import xlrd
-from rdflib import Graph, Namespace, FOAF, XSD, RDF, RDFS
+from rdflib import Graph, Namespace, FOAF, XSD, RDF, RDFS, URIRef, Literal
 import os.path
+import unicodedata
 from enum import Enum
 
+ONTODIR = "./iec/"
 SUBDIR = "./iec/pollutions/"
 
-FILES = { 'Данные бар Ярки Сев Байкал.xls':1,
-          'Данные Харанцы Ольхон.xls':2,
-          'Данные Хужир Ольхон.xls':3 }
+
+TARGET = 'import.ttl'
+TARGETMT = 'PeriodicTable.ttl'
 
 EQUIPMENT = "S8 Tiger"
 EQUIPMENT_TYPE = "X-Ray fluorescence analysis"
 
 PT = Namespace('https://crust.irk.ru/ontology/pollution/terms/1.0/')
 P = Namespace('https://crust.irk.ru/ontology/pollution/1.0/')
+MT = Namespace('http://www.daml.org/2003/01/periodictable/PeriodicTable#')
 
 G = Graph(bind_namespaces="rdflib")
-G.bind('pt', PT)
-G.bind('pi', P)
+GMT = Graph(bind_namespaces="rdflib")
+GMT.parse(location=os.path.join(ONTODIR, 'PeriodicTable.owl'))
+GS = [G, GMT]
+
+for _ in GS:
+    _.bind('pt', PT)
+    _.bind('pi', P)
+    _.bind('mt', MT)
+
+def normURI(s):
+    r = ""
+    for c in s:
+        d = unicodedata.category(c)
+        if d[0] in ['L','N']:
+            r+=c
+        elif len(r)>0 and r[-1]!='_':
+            r+='_'
+    return r.rstrip('_')
 
 class State(Enum):
     NONE = 0
@@ -28,14 +47,26 @@ class State(Enum):
     IGNORE = 4
 
 class ImpState:
-    def __init__(self):
+    _sample_iri_ = PT['sample']
+    def __init__(self, graph, datasetIRI):
         self.state = State.NONE
         self.instr = False
+        self.cls = {}
+        self.header = {}
+        self.dsiri = datasetIRI
+        self.g = graph
+        self.sample = None
+        self.sample_col = None
 
     def data(self):
         return self.state == State.DATA
+    def ign(self):
+        return self.state == State.IGNORE
 
-    def r(self, row, col):
+    def r(self, row, rx):
+        self.instr = False
+        self.sample = None
+
         f = row[0]
         fs = f.value
 
@@ -49,26 +80,81 @@ class ImpState:
                     print("#! ERROR Key: {}".format(k))
                     quit()
                 self.instr = True
+        if self.ign():
+            return
 
         if self.data() and not self.instr:
             # print("DT: {}".format(row))
+            if self.sample is None:
+                # print(self.header)
+                self.c(row[self.sample_col], rx, self.sample_col)
             for i, cell in enumerate(row):
-                self.c(cell, i, col)
+                self.c(cell, rx, i)
+            return
 
-        self.instr = False
+        if self.state == State.HEADER and not self.instr:
+            for i, cell in enumerate(row):
+                self.h(cell, rx, i)
+            return
+
 
     def c(self, cell, row, col):
-        pass
+        if cell.ctype in [xlrd.XL_CELL_EMPTY, xlrd.XL_CELL_BLANK]:
+            return
+        try:
+            field = self.header[col]
+        except KeyError as k:
+            print("#! ERROR header key {} not in {}".format(k, self.header))
+            # quit()
+            return
+        add = self.g.add
+        ds = self.dsiri
+        if field == self._sample_iri_:
+            self.sample = P[cell.value.replace(' ','')]
+            add((ds, self._sample_iri_, self.sample))
+        elif self.sample is not None:
+            add((self.sample, PT[field], Literal(cell.value)))
+        else:
+            print('#! ERROR: nowhere to store {} R:{} C:{}\n#!{}'.format(cell, row, col, self.header))
+            quit()
+
+    def h(self, cell, row, col):
+        # print(cell, col)
+        if cell.ctype in [xlrd.XL_CELL_EMPTY, xlrd.XL_CELL_BLANK]:
+            return
+
+        name = normURI(str(cell.value))
+        add = self.g.add
+        ds = self.dsiri
+        if name in self._sample_names_:
+            name = self._sample_iri_
+            self.sample_col = col
+
+        self.header[col] = name
+
+class Yarki(ImpState):
+    _sample_names_ = ['Sample', 'Полевой']
+
+class Kharantsy(ImpState):
+    _sample_names_ = ['Номер_пробы']
+
+class Khuzhir(Kharantsy):
+    pass
 
 
-def parse_sheet(sh):
+FILES = { 'Данные бар Ярки Сев Байкал.xls': Yarki,
+          'Данные Харанцы Ольхон.xls': Kharantsy,
+          'Данные Хужир Ольхон.xls': Khuzhir }
+
+
+def parse_sheet(sh, sheetIRI, constr):
     # print("Cell D30 is {0}".format(sh.cell_value(rowx=29, colx=3)))
-    st = ImpState()
+    st = constr(G, sheetIRI)
     for rx in range(sh.nrows):
         st.r(sh.row(rx), rx)
 
 
-def parse_xl(file):
+def parse_xl(file, constr):
     print("# FILE: {} at {}".format(file, SUBDIR))
     pathfile = os.path.join(SUBDIR, file)
     # df = pd.read_excel(pathfile)
@@ -77,10 +163,15 @@ def parse_xl(file):
     for sheet in wb.sheet_names():
         print("# Wb: {}, sheet: {}". format(file, sheet))
         sh = wb.sheet_by_name(sheet)
+        sheetname = normURI(file+"_"+sheet)
         print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
-        parse_sheet(sh)
+        parse_sheet(sh, P[sheetname], constr)
 
 
 if __name__=="__main__":
-    for file in FILES:
-        parse_xl(file)
+    for file, constr in FILES.items():
+        parse_xl(file, constr)
+    with open(TARGET, "w") as o:
+        o.write(G.serialize(format='turtle'))
+    with open(TARGETMT, "w") as o:
+        o.write(GMT.serialize(format='turtle'))
